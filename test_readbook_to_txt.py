98c7f7ab97aa59931b55ec81c82e9a97972b3330
jeffery0327 <jeffeeygrace@gmail.com>
2021-10-21 00:16:38 +0800

    0.0.4

--- test_readbook_to_txt.py
+++ test_readbook_to_txt.py
@@ -6,5 +6,18 @@ content = content.replace("　", "")
 # content = content.replace("\n", "")
 sentences = content.split("\n")
 
+import jieba
+import paddle
 
-print(sentences)
+paddle.enable_static()#paddle 2.x default改成dynamic，所以要初始設定static
+jieba.enable_paddle()# 启动paddle模式。 0.40版之后开始支持，早期版本不支持
+
+f=open("test_writebook.txt",'a',encoding='UTF-8')
+
+
+
+for i in range(0,len(sentences)):
+    seg_list = jieba.cut(sentences[i], cut_all=True)
+    f.write("Full Mode: " + "/ ".join(seg_list))
+
+# print(sentences[0])
