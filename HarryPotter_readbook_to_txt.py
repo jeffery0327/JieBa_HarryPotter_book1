4a308ac0ad3cb8c5430c3de1c617ae899974fdb8
jeffery0327 <jeffeeygrace@gmail.com>
2021-10-22 19:41:10 +0800

    0.0.6

--- HarryPotter_readbook_to_txt.py
+++ HarryPotter_readbook_to_txt.py
@@ -7,6 +7,7 @@ f.close()
 
 content = content.replace("　", "")
 
+
 import jieba
 import paddle
 import jieba.posseg
@@ -15,7 +16,6 @@ import jieba.analyse
 paddle.enable_static()#paddle 2.x default改成dynamic，所以要初始設定static
 jieba.enable_paddle()# 启动paddle模式。 0.40版之后开始支持，早期版本不支持
 jieba.load_userdict("Lib_HarryPotter_people.txt")#匯入自製人名字典
-
 
 
 f=open("HarryPotter_content_words.txt",'w',encoding='UTF-8')
@@ -27,3 +27,5 @@ f.write(",".join(seg_list))
 print("analyse:")
 for x, w in jieba.analyse.extract_tags(content,topK=100, withWeight=True):
     print('%s %s' % (x, w))
+
+
